c1fc9025ccbb851965c0db16ac938cf54cc94500
Dennis Wong <cfwwong@gmail.com>
2022-09-29 21:16:02 +0100

    UserInterface 1st test case ready

--- SeatAllocateService.Tests/UserInterfaceTests.cs
+++ SeatAllocateService.Tests/UserInterfaceTests.cs
@@ -2,14 +2,18 @@
 
 public class UserInterfaceTests
 {
+    private UserInterface _userInterface;
+
     [SetUp]
     public void Setup()
     {
+        _userInterface = new UserInterface();
     }
 
     [Test]
-    public void Test1()
+    public void Calling_AskNumSeatsNeeded_Should_Return_An_Int_Between_1_And_3()
     {
-        Assert.Pass();
+        int numSeatNeeded = _userInterface.AskNumSeatsNeeded();
+        Assert.IsTrue(numSeatNeeded.Equals(1) || numSeatNeeded.Equals(2) || numSeatNeeded.Equals(3));
     }
 }
