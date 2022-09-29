47f720aa4b3a9eaa75514f659dcbce3585c7c7c5
Dennis Wong <cfwwong@gmail.com>
2022-09-29 21:27:43 +0100

    UserInterface 2nd test case ready

--- SeatAllocateService.Tests/UserInterfaceTests.cs
+++ SeatAllocateService.Tests/UserInterfaceTests.cs
@@ -16,4 +16,19 @@ public class UserInterfaceTests
         int numSeatNeeded = _userInterface.AskNumSeatsNeeded();
         Assert.IsTrue(numSeatNeeded.Equals(1) || numSeatNeeded.Equals(2) || numSeatNeeded.Equals(3));
     }
+
+    [Test]
+    public void Given_Reaining_Seats_Info_Should_Display_On_Console()
+    {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        List<Seat> seats = new List<Seat>();
+        seats.Add(new Seat('C',4,'A'));
+        seats.Add(new Seat('C',5,'A'));
+        _userInterface.DisEmptySeats(seats);
+
+        var output = stringWriter.ToString();
+        Assert.That(output, Is.EqualTo("\nThe Mars Rover coordinates and facing are:\nMars Rover - MR0: 2 1 E\n\n"));
+    }
 }
