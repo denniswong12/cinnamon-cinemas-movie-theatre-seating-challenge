397bd7ebf8dd32d2d8a0f39b9a36bb9938792975
Dennis Wong <cfwwong@gmail.com>
2022-09-29 21:18:19 +0100

    UserInterface 1st test case passed.

--- SeatAllocateService/UserInterface.cs
+++ SeatAllocateService/UserInterface.cs
@@ -1,10 +1,19 @@
-﻿using System;
-namespace SeatAllocateService
+﻿namespace SeatAllocateService
 {
     public class UserInterface
     {
+        protected int NumSeatsNeeded { get; private set; }
+
         public UserInterface()
         {
+        }
+
+        public int AskNumSeatsNeeded()
+        {
+            Random rnd = new Random();
+            var numSeatsNeeded = rnd.Next(1, 4);
+            NumSeatsNeeded = numSeatsNeeded;
+            return numSeatsNeeded;
         }
     }
 }
