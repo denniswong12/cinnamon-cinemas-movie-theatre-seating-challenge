22610078ce5074319dec143f032bc88cf9139133
Dennis Wong <cfwwong@gmail.com>
2022-09-30 12:12:56 +0100

    ReservationCenter 2nd test case ready.

--- SeatAllocateService.Tests/ReservationCenterTests.cs
+++ SeatAllocateService.Tests/ReservationCenterTests.cs
@@ -16,4 +16,11 @@ public class ReservationCenterTests
         _reservationCenter.InitHall();
         _reservationCenter._seats.Count().Should().Be(15);
     }
+
+    [Test]
+    public void Get_Number_Of_Seats_Needed_Should_Return_Integer_Between_1_And_3()
+    {
+        int numSeatsNeeded = _reservationCenter.GetNumOfSeatsNeeded();
+        Assert.IsTrue(numSeatsNeeded.Equals(1) || numSeatsNeeded.Equals(2) || numSeatsNeeded.Equals(3));
+    }
 }
